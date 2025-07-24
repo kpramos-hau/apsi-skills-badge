2164ce712654bc37ac07673b2300d0338fed55fa
jmmacayanan <150020298+rejj0925@users.noreply.github.com>
2025-07-24 15:52:36 +0800

    Added reporting page; added username, email, and company name in re cords; sidebar can now navigate to reporting page

--- src/pages/ReportingPage.js
+++ src/pages/ReportingPage.js
@@ -1,5 +1,56 @@
-function ReportingPage(){
+import { useState, useEffect } from 'react'
+import '../styles/reporting.css'
 
+function ReportingPage(){
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const API_URL = 'https://jsonplaceholder.typicode.com/users';
+
+    // ---------- GET: Fetch all users ----------
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    const fetchUsers = async () => {
+        setLoading(true);
+        setError(null);
+        try {
+        const res = await fetch(API_URL);
+        if (!res.ok) throw new Error('Failed to fetch users');
+        const data = await res.json();
+        setUsers(data);
+        } catch (err) {
+        setError(err.message);
+        } finally {
+        setLoading(false);
+        }
+    };
+
+    return(
+        <div className="main">
+            <div className="record-container">
+                <table>
+                       <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Username</th>
+                            <th>Email</th>
+                            <th>Company</th>
+                       </tr>
+                    {users.map(user => (
+                       <tr key={user.id}>
+                            <td><strong>{user.id}</strong></td>
+                            <td>{user.name}</td>
+                            <td>{user.username}</td>
+                            <td>{user.email}</td>
+                            <td>{user.company['name']}</td>
+                       </tr>
+                    ))}
+                </table>
+            </div>
+        </div>
+    )
 }
 
 export default ReportingPage;
