21e301846e9a6fd8a6b7c963bf74a514e59360c0
kevinzekee <kzekeramos@gmail.com>
2025-07-29 22:31:35 +0800

    Fix login page design

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -16,22 +16,32 @@ function LoginPage(){
     }
     // Navigation
     return(
-        <div className="main-login ">  
-            <div className="login-element animate__animated animate__fadeIn animate__faster">
-                <h1>Login</h1>
-                <div className='input-container'>
+        <div className="main-login">  
+            <div className='left-login'>
+                <h1 className='login-text-formal'>"Top #41 Country Rank in Osu! BTW."</h1>
+                <div className='text-image-container'> 
+                    <p>-</p>
+                    <img src="https://a.ppy.sh/12028437?1753715495.jpeg" className='img-handle' alt='MiggiePic'></img>
+                    <a className='p-text-formal'> @Miggies (Developer)</a>
+                </div>
+                </div>
+            <div className='right-login'>
+                <div className='login-container'>
+                <h1 className="login-text animate__animated animate__fadeIn animate__faster">Login</h1>
+                    <label className='label-text'>Username</label>
                     <input className='text-box' type="text" placeholder="Username"></input>
+                    <label className='label-text'>Password</label>
                     <input className='text-box' type="password" placeholder="Password"></input>
-                </div>
-                <div className='options'>
-                    <div className='options-left'>
-                        <input type='checkbox'></input><label id="link" >Remember Me?</label>
-                    </div>
+                    <div className='options'>
+                        <div className='options-left'>
+                            <input type='checkbox'></input><label id="link" >Remember Me?</label>
+                        </div>
                     
-                    <a id="link" href='#'>Forgot Pasword?</a>
+                        <a id="link" href='#'>Forgot Pasword?</a>
+                    </div>
+                    <button className='sign-in' onClick={handleDashboard}>Sign In</button>
+                    <a id="link" onClick={handleLanding} href='#'>Home</a>
                 </div>
-                <button className='sign-in' onClick={handleDashboard}>Sign In</button>
-                <a id="link" onClick={handleLanding} href='#'>Home</a>
             </div>
         </div>
     )
